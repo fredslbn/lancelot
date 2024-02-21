81496ff5a0a08ad440a65641c91e12734854af9b
fredslbn <fh.silaban@gmail.com>
2024-02-21 23:43:21 +0700

    build for merlin

--- .circleci/clanglinaro.sh
+++ .circleci/clanglinaro.sh
@@ -11,7 +11,7 @@ KERNEL_DIR="$(pwd)"
 ##----------------------------------------------------------##
 # Device Name and Model
 MODEL=Xiaomi
-DEVICE=lava
+DEVICE=merlin
 
 # Kernel Version Code
 #VERSION=
@@ -41,7 +41,7 @@ DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%T")
 TANGGAL=$(date +"%F%S")
 
 # Specify Final Zip Name
-ZIPNAME="SUPER.KERNEL-LANCELOT-(clang-linaro)-$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M").zip"
+ZIPNAME="SUPER.KERNEL-MERLIN-(clang-linaro)-$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M").zip"
 FINAL_ZIP=${ZIPNAME}-${DEVICE}-${TANGGAL}.zip
 
 ##----------------------------------------------------------##
